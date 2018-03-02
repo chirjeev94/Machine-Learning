6a3de90272116cede42b4af87972da159feb5fe3
chintan94 <cd2966@columbia.edu>
2018-03-02 02:06:36 -0500

    Fixing setup

--- hw2/hw2q5.py
+++ hw2/hw2q5.py
@@ -1,1 +1,136 @@
-#!/usr/bin/env python2# -*- coding: utf-8 -*-"""Created on Wed Feb 28 01:11:54 2018@author: chintan94"""import pandas as pdimport numpy as npimport gcfrom nltk.corpus import stopwordsfrom collections import Counterfrom time import timetotal_len = 0tf_idf_denom = 0w0 = 0idf_w_0 = 0def unique_word_set(string_list):    set_list = []    for string in string_list:        set_list.append(set(string.split(" ")))    unique_words = set.union(*set_list)    # If the line below fails, download stopwords    # import nltk    # nltk.download('stopwords')    stop_words = set(stopwords.words('english'))    unique_words = unique_words - stop_words    w0 = pd.Series(0, index=unique_words)    w0.to_csv("init_w.csv")    return w0def faster_tfd(chunk):    chunk["text"] = chunk["text"].str.split()    chunk["text"] = chunk["text"].map(lambda x: pd.Series.value_counts(pd.Series(x)))    return chunkdef perceptron_map_tf(row):    global w0, tf_idf_denom    vec = pd.Series(row[1], index=w0.index).fillna(0.0)    tf_idf_denom = tf_idf_denom + vec    dot_product = w0.dot(vec)    if (dot_product >= 0 and row[0] == 0):        w0 = w0 - vec    elif (dot_product <= 0 and row[0] == 1):        w0 = w0 + vecdef perceptron_map_tf_idf(row):    global idf_w_0, tf_idf_denom    vec = pd.Series(row[1], index=w0.index).fillna(0.0)    vec = vec / tf_idf_denom * total_len / 2    dot_product = w0.dot(vec)    if (dot_product >= 0 and row[0] == 0):        idf_w_0 = idf_w_0 - vec    elif (dot_product <= 0 and row[0] == 1):        idf_w_0 = idf_w_0 + vecdef main():    global tf_idf_denom, total_len, w0, idf_w_0    # Storing all the unique words in a files    # Toggle it to true if it's first run    uw_comp_necessary = False    if uw_comp_necessary:        # no need to load the entire file        df = pd.read_csv("reviews_tr.csv")        string_list = df["text"]        w0 = unique_word_set(string_list)        del df        gc.collect()    else:        w0 = pd.Series.from_csv("init_w.csv")    tf_idf_denom = pd.Series(0.0, index=w0.index)    idf_w_0 = pd.Series(0.0, index=w0.index)    w_list = []    idf_w_list = []    chunksize = 10000    control = True    write = True    tf_idf_flag = False    # Processing the file in chunck    start = time()    for chunk in pd.read_csv("reviews_tr.csv", chunksize=chunksize):        total_len += len(chunk)        # TFD        chunk = faster_tfd(chunk)        print "TFD Computed " + str(time() - start)        chunk.apply(perceptron_map_tf, axis=1)        print "Shuffling " + str(time() - start)        chunk = chunk.iloc[np.random.permutation(len(chunk))]        chunk.apply(perceptron_map_tf, axis=1)        # TF_IDF        print "TF_IDF Start " + str(time() - start)        chunk.apply(perceptron_map_tf_idf, axis=1)        print "TF_IDF_Shuffling " + str(time() - start)        chunk = chunk.iloc[np.random.permutation(len(chunk))]        chunk.apply(perceptron_map_tf_idf, axis=1)        print "TF_IDF Part Over " + str(time() - start)        if (total_len % 10 ** 4) == 0:            w_list.append(w0 / (total_len * 2 + 1))            idf_w_list.append(idf_w_0 / (1 + total_len * 2))            print "Processed " + str(total_len / 2) + " in " + str(time() - start)            if control:                print "Do you wish to continue?"                ans = raw_input("Yes/No?")                if ans == 'No':                    break    # Documenting all the w for testing    # To get it back use this    # w = pd.Series.from_csv("w0.csv")    j = 0    if (write):        for i in w_list:            i.to_csv("w" + str(j) + ".csv")            j = j + 1        j = 0        for i in idf_w_list:            i.to_csv("idf_w" + str(j) + ".csv")            j = j + 1    # Testing accuracy part to followif __name__ == '__main__':    main()
+"""
+Created on Wed Feb 28 01:11:54 2018
+
+@author: chintan94
+"""
+
+import pandas as pd
+import numpy as np
+import gc
+from nltk.corpus import stopwords
+from collections import Counter
+from time import time
+
+total_len = 0
+tf_idf_denom = 0
+w0 = 0
+idf_w_0 = 0
+
+
+def unique_word_set(string_list):
+    set_list = []
+    for string in string_list:
+        set_list.append(set(string.split(" ")))
+    unique_words = set.union(*set_list)
+    # If the line below fails, download stopwords
+    # import nltk
+    # nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+    unique_words = unique_words - stop_words
+    w0 = pd.Series(0, index=unique_words)
+    w0.to_csv("init_w.csv")
+    return w0
+
+
+def faster_tfd(chunk):
+    chunk["text"] = chunk["text"].str.split()
+    chunk["text"] = chunk["text"].map(lambda x: pd.Series.value_counts(pd.Series(x)))
+    return chunk
+
+
+def perceptron_map_tf(row):
+    global w0, tf_idf_denom
+    vec = pd.Series(row[1], index=w0.index).fillna(0.0)
+    tf_idf_denom = tf_idf_denom + vec
+    dot_product = w0.dot(vec)
+    if (dot_product >= 0 and row[0] == 0):
+        w0 = w0 - vec
+    elif (dot_product <= 0 and row[0] == 1):
+        w0 = w0 + vec
+
+
+def perceptron_map_tf_idf(row):
+    global idf_w_0, tf_idf_denom
+    vec = pd.Series(row[1], index=w0.index).fillna(0.0)
+    vec = vec / tf_idf_denom * total_len / 2
+    dot_product = w0.dot(vec)
+    if (dot_product >= 0 and row[0] == 0):
+        idf_w_0 = idf_w_0 - vec
+    elif (dot_product <= 0 and row[0] == 1):
+        idf_w_0 = idf_w_0 + vec
+
+
+def main():
+    global tf_idf_denom, total_len, w0, idf_w_0
+    # Storing all the unique words in a files
+    # Toggle it to true if it's first run
+    uw_comp_necessary = False
+
+    if uw_comp_necessary:
+        # no need to load the entire file
+        df = pd.read_csv("reviews_tr.csv")
+        string_list = df["text"]
+        w0 = unique_word_set(string_list)
+        del df
+        gc.collect()
+    else:
+        w0 = pd.Series.from_csv("init_w.csv")
+
+    tf_idf_denom = pd.Series(0.0, index=w0.index)
+    idf_w_0 = pd.Series(0.0, index=w0.index)
+    w_list = []
+    idf_w_list = []
+    chunksize = 10000
+    control = True
+    write = True
+    tf_idf_flag = False
+
+    # Processing the file in chunck
+    start = time()
+    for chunk in pd.read_csv("reviews_tr.csv", chunksize=chunksize):
+
+        total_len += len(chunk)
+        # TFD
+        chunk = faster_tfd(chunk)
+        print "TFD Computed " + str(time() - start)
+        chunk.apply(perceptron_map_tf, axis=1)
+        print "Shuffling " + str(time() - start)
+        chunk = chunk.iloc[np.random.permutation(len(chunk))]
+        chunk.apply(perceptron_map_tf, axis=1)
+
+        # TF_IDF
+        print "TF_IDF Start " + str(time() - start)
+        chunk.apply(perceptron_map_tf_idf, axis=1)
+        print "TF_IDF_Shuffling " + str(time() - start)
+        chunk = chunk.iloc[np.random.permutation(len(chunk))]
+        chunk.apply(perceptron_map_tf_idf, axis=1)
+        print "TF_IDF Part Over " + str(time() - start)
+
+        if (total_len % 10 ** 4) == 0:
+            w_list.append(w0 / (total_len * 2 + 1))
+            idf_w_list.append(idf_w_0 / (1 + total_len * 2))
+            print "Processed " + str(total_len / 2) + " in " + str(time() - start)
+
+            if control:
+                print "Do you wish to continue?"
+                ans = raw_input("Yes/No?")
+                if ans == 'No':
+                    break
+
+    # Documenting all the w for testing
+    # To get it back use this
+    # w = pd.Series.from_csv("w0.csv")
+    j = 0
+    if (write):
+        for i in w_list:
+            i.to_csv("w" + str(j) + ".csv")
+            j = j + 1
+        j = 0
+        for i in idf_w_list:
+            i.to_csv("idf_w" + str(j) + ".csv")
+            j = j + 1
+
+    # Testing accuracy part to follow
+
+if __name__ == '__main__':
+    main()
