00039cceb71b34047c2e05e83fffd77ed7bf912a
chintan94 <cd2966@columbia.edu>
2018-03-19 22:55:32 -0400

    Naming changes

--- reviews_sentiment_perceptron/test_biggram.py
+++ reviews_sentiment_perceptron/test_biggram.py
@@ -11,9 +11,7 @@ import pandas as pd
 import numpy as np
 import gc
 from nltk.corpus import stopwords
-from collections import Counter
-from time import time
-
+import matplotlib.pyplot as plt
 w0 = 0
 stop_words = set(stopwords.words('english'))
 
@@ -40,12 +38,12 @@ def biggram():
     w0 = pd.Series.from_csv("100_biggram_init_w.csv")
     test_data = pd.read_csv("reviews_tr.csv")
     tf_w_list = []
-    for i in range(1,20):
+    for i in range(0,14):
         tf_w_list.append(pd.Series.from_csv("biggram_w" + str(i) + ".csv"))
 
-    sample1 = test_data.sample(n=20000)
-    sample2 = test_data.sample(n=20000)
-    sample3 = test_data[len(test_data) - 20000:]
+    sample1 = test_data.sample(n=15000)
+    sample2 = test_data.sample(n=15000)
+    sample3 = test_data.sample(n=15000)
 
     sample1 = biggram_generator(sample1)
     sample2 = biggram_generator(sample2)
@@ -61,15 +59,17 @@ def biggram():
     for i in tf_w_list:
         temp = sample1["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample1["label"]
-        accu1.append(len([m for m in temp if m > 0]) / len(sample1))
+        accu1.append(float(len([m for m in temp if m > 0])) / float(len(sample1)))
         temp = sample2["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample2["label"]
-        accu2.append(len([m for m in temp if m > 0]) / len(sample2))
+        accu2.append(float(len([m for m in temp if m > 0])) / float(len(sample2)))
         temp = sample3["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample3["label"]
-        accu3.append(len([m for m in temp if m > 0]) / len(sample3))
-        accu = [x + y + z for x, y, z in zip(accu1, accu2, accu3)]
-
+        accu3.append(float(len([m for m in temp if m > 0])) / float(len(sample3)))
+        accu = [(x + y + z)/3.0 for x, y, z in zip(accu1, accu2, accu3)]
+    print "Done"
+    del sample1,sample2,sample3
+    gc.collect()
     return accu
 
 
@@ -78,11 +78,11 @@ def tf_idf():
     w0 = pd.Series.from_csv("new_init_w.csv")
     test_data = pd.read_csv("reviews_tr.csv")
     tf_w_list = []
-    for i in range(1,40,2):
+    for i in range(1,30,2):
         tf_w_list.append(pd.Series.from_csv("idf_w" + str(i) + ".csv"))
-    sample1 = test_data.sample(n=20000)
-    sample2 = test_data.sample(n=20000)
-    sample3 = test_data[len(test_data) - 20000:]
+    sample1 = test_data.sample(n=15000)
+    sample2 = test_data.sample(n=15000)
+    sample3 = test_data.sample(n=15000)
 
     sample1 = faster_tfd(sample1)
     sample2 = faster_tfd(sample2)
@@ -98,15 +98,17 @@ def tf_idf():
     for i in tf_w_list:
         temp = sample1["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample1["label"]
-        accu1.append(len([m for m in temp if m > 0]) / len(sample1))
+        accu1.append(float(len([m for m in temp if m > 0])) / float(len(sample1)))
         temp = sample2["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample2["label"]
-        accu2.append(len([m for m in temp if m > 0]) / len(sample2))
+        accu2.append(float(len([m for m in temp if m > 0])) / float(len(sample2)))
         temp = sample3["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample3["label"]
-        accu3.append(len([m for m in temp if m > 0]) / len(sample3))
-        accu = [x + y + z for x, y, z in zip(accu1, accu2, accu3)]
-
+        accu3.append(float(len([m for m in temp if m > 0])) / float(len(sample3)))
+        accu = [(x + y + z)/3.0 for x, y, z in zip(accu1, accu2, accu3)]
+    print "Done"
+    del sample1,sample2,sample3
+    gc.collect()
     return accu
 
 
@@ -115,13 +117,13 @@ def tfd_test():
     w0 = pd.Series.from_csv("new_init_w.csv")
     test_data = pd.read_csv("reviews_tr.csv")
     tf_w_list = []
-    for i in range(1, 40, 2):
+    for i in range(1, 30, 2):
         tf_w_list.append(pd.Series.from_csv("w" + str(i) + ".csv"))
 
-    sample1 = test_data.sample(n=20000)
-    sample2 = test_data.sample(n=20000)
-    sample3 = test_data[len(test_data) - 20000:]
-
+    sample1 = test_data.sample(n=15000)
+    sample2 = test_data.sample(n=15000)
+    sample3 = test_data.sample(n=15000)
+    
     sample1 = faster_tfd(sample1)
     sample2 = faster_tfd(sample2)
     sample3 = faster_tfd(sample3)
@@ -133,18 +135,21 @@ def tfd_test():
     sample1["label"] = sample1["label"].replace(to_replace=0, value=-1)
     sample2["label"] = sample2["label"].replace(to_replace=0, value=-1)
     sample3["label"] = sample3["label"].replace(to_replace=0, value=-1)
+    
     for i in tf_w_list:
         temp = sample1["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample1["label"]
-        accu1.append(len([m for m in temp if m > 0]) / len(sample1))
+        accu1.append(float(len([m for m in temp if m > 0])) / float(len(sample1)))
         temp = sample2["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample2["label"]
-        accu2.append(len([m for m in temp if m > 0]) / len(sample2))
+        accu2.append(float(len([m for m in temp if m > 0])) / float(len(sample2)))
         temp = sample3["text"].apply(lambda x: np.dot(x, i))
         temp = temp * sample3["label"]
-        accu3.append(len([m for m in temp if m > 0]) / len(sample3))
-        accu = [x + y + z for x, y, z in zip(accu1, accu2, accu3)]
-
+        accu3.append(float(len([m for m in temp if m > 0])) / float(len(sample3)))
+        accu = [(x + y + z)/3.0 for x, y, z in zip(accu1, accu2, accu3)]
+    print "Done"
+    del sample1,sample2,sample3
+    gc.collect()
     return accu
 
 
@@ -153,6 +158,20 @@ def main():
     bigg = biggram()
     tfd = tfd_test()
     tfidf = tf_idf()
+
+    plt.title("Test Accuracy -> Datasize")
+    plt.plot(range(100000,800000,50000), bigg, label="Biggram",marker='o')
+    plt.plot(range(100000,800000,50000), tfd, label="tf-idf",marker='o')
+    plt.plot(range(100000,800000,50000), tfidf, label="Onegram",marker='o')
+    plt.xlabel("Test data size->")
+    plt.ylabel("Accuracy->")
+    plt.legend()
+    plt.gcf().set_size_inches(15,8)
+    plt.legend()
+    plt.show()
+    plt.savefig("graph_hw2q5.png")
+
+
 
 if __name__ == '__main__':
     main()
